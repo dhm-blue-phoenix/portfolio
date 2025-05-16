87cec62dfde65b6025b472c06486a0ba8f4d1cec
Dominik M <phoenix.dh-m16@outlook.com>
2025-05-16 21:31:42 +0200

    add main section why me

--- src/app/lang/en-lang.service.ts
+++ src/app/lang/en-lang.service.ts
@@ -16,6 +16,19 @@ export class EnLangService {
       },
       'navbar': {
         'menu': ['Why me', 'Skills', 'Projects', 'Contact']
+      },
+      'main': {
+        'sections': {
+          'why_me': {
+            'headline': 'Why me',
+            'left_container': {
+              'first_text': 'I am',
+              'last_text': 'located in Munich..|'
+            },
+            'right_container': 'Why are you passionate about coding? You can include some key traits like: analytical thinking, creativity, persistence and collaboration. A problem-solving mindset is always nice to have.',
+            'button': 'Lets talk'
+          }
+        }
       }
     }
   };
